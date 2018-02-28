ebd0f915b4e212f37565ecd326735ecd9f15b8b5
Stéphane Raimbault <stephane.raimbault@gmail.com>
2018-02-28 12:53:26 +0100

    Remove PWA plugin, JS and assets (useless) that breaks e2e
    https://github.com/vuejs/vue-cli/issues/893

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
-import './registerServiceWorker'
 import './stylus/main.styl'
 
 Vue.config.productionTip = false
