1217cb027320b24ae3204cdf57277fff209db7e5
Stéphane Raimbault <stephane.raimbault@gmail.com>
2018-03-01 14:56:13 +0100

    Rendering of companies from mock

---
+++ src/components/CompanyCard.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  v-card(color="grey lighten-4")
+    v-card-title(primay-title)
+      div
+        div(class="headline") {{ company.line1 }}
+        span {{ company.address }}
+        br
+        span {{ company.zipCode }} {{ company.city }}
+        br
+        span {{ company.codeApe }}
+        br
+        span {{ company.textApe }}
+    v-card-actions
+      v-btn(flat, color="primary", @click="onSelectCompany") Sélectionner
+</template>
+
+<script>
+export default {
+  name: 'CompanyCard',
+  props: {
+    company: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    onSelectCompany () {
+      this.$emit('select', {'siret': this.company.siret})
+    }
+  }
+}
+</script>
--- src/views/Subscribe.vue
+++ src/views/Subscribe.vue
@@ -12,35 +12,105 @@
                 v-flex(xs12, md6)
                   v-text-field(v-model="siren", label="Numéro de SIREN ou SIRET")
                   v-btn(color="primary" @click.native="searchSiren") Suivant
-                  v-btn(flat) Annuler
           v-stepper-step(step="2", :complete="step > 2") Sélection de l'établissement
           v-stepper-content(step="2")
             v-progress-circular(v-if="isSearching", indeterminate, color="primary")
-            v-btn(color="primary" @click.native="step = 3") Suivant
-            v-btn(flat)
+            v-container(v-else, fluid, grid-list-lg)
+              v-layout(row, wrap)
+                v-flex(xs12, sd6, md4, v-for="company in companies", :key="company.siret")
+                  company-card(:company="company", @select="selectCompany")
           v-stepper-step(step="3", :complete="step > 3") Mise à jour de vos coordonnées
           v-stepper-content(step="3")
             v-card(class="mb-5")
+
             v-btn(color="primary" @click.native="step = 4") Suivant
-            v-btn(flat)
 </template>
 
 <script>
+import CompanyCard from '@/components/CompanyCard.vue'
+
 export default {
   data () {
     return {
       siren: null,
       step: 1,
-      isSearching: false
+      isSearching: false,
+      companies: [],
+      selectedCompanySiret: '',
     }
+  },
+
+  components: {
+    'company-card': CompanyCard
   },
 
   methods: {
     searchSiren () {
       this.isSearching = true
       this.step = 2
-      // this.searching = false
+
+      const rawCompanies = [
+        {
+          siren: '824808349',
+          nic: '00016',
+          l1_normalisee: 'MADAME CLOTILDE TESSIER',
+          l6_normalisee: '75008 PARIS',
+          numvoie: '9',
+          typevoie: 'RTE',
+          libvoie: 'DE ROCHEFORT',
+          codpos: '75008',
+          dcret: '20170106',
+          apet700: '4779Z',
+          libapet: "Commerce de détail de biens d'occasion en magasin"
+        },
+        {
+          siren: '824808349',
+          nic: '00017',
+          l1_normalisee: 'MADAME BRIGITTE TESSIER',
+          l6_normalisee: '75008 PARIS',
+          numvoie: '9',
+          typevoie: 'RTE',
+          libvoie: 'DE ROCHEFORT',
+          codpos: '75008',
+          dcret: '20170106',
+          apet700: '4779Z',
+          libapet: "Commerce de détail de biens d'occasion en magasin"
+        },
+        {
+          siren: '824808349',
+          nic: '00018',
+          l1_normalisee: 'MONSIEUR MARC BOULLAIS',
+          l6_normalisee: '75008 PARIS',
+          numvoie: '9',
+          typevoie: 'RTE',
+          libvoie: 'DE ROCHEFORT',
+          codpos: '75008',
+          dcret: '20170106',
+          apet700: '4779Z',
+          libapet: "Commerce de détail de biens d'occasion en magasin"
+        }
+      ]
+      this.companies = []
+      for (let rawCompany of rawCompanies) {
+        rawCompany.l6_normalisee = rawCompany.l6_normalisee || ''
+        this.companies.push({
+          siret: rawCompany.siren + rawCompany.nic,
+          line1: rawCompany.l1_normalisee,
+          address: `${rawCompany.numvoie} ${rawCompany.typevoie} ${rawCompany.libvoie}`,
+          zipCode: rawCompany.codpos,
+          city: rawCompany.l6_normalisee.replace(rawCompany.codpos + ' ', ''),
+          codeApe: rawCompany.apet700,
+          textApe: rawCompany.libapet,
+        })
+      }
+      this.isSearching = false
+    },
+
+    selectCompany (data) {
+      this.selectedCompanySiret = data.siret
+      this.step = 3
     }
+
   }
 }
 </script>
