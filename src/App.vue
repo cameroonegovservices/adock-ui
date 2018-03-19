e8ae1b36a2706a543cbb860ba9b4e3e1a0135857
Stéphane Raimbault <stephane.raimbault@gmail.com>
2018-03-19 15:30:32 +0100

    Move 'A Dock' on the left of the toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
 <template lang="pug">
   v-app
     v-toolbar(app, fixed, light)
-      v-spacer
+      img(src='./assets/marianne.svg', width='80px')
       v-toolbar-title A Dock
     v-content
       router-view
 </template>
-
-<style scoped>
-.toolbar {
-  background-image: url(assets/marianne.svg);
-  background-size: 80px;
-  background-repeat: no-repeat;
-  background-position: 1em;
-}
-</stylet>
