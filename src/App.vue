29d4e9e5440460aadb995347356ff2b555f70240
Stéphane Raimbault <stephane.raimbault@gmail.com>
2018-03-19 15:34:21 +0100

    New baseline and click on logo to go home

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
   v-app
     v-toolbar(app, fixed, light)
-      img(src='./assets/marianne.svg', width='80px')
-      v-toolbar-title A Dock
+      router-link(:to="{name: 'search'}")
+        img(src='./assets/marianne.svg', width='80px')
+      v-toolbar-title A Dock - Simplifions les relations transporteurs/chargeurs
     v-content
       router-view
 </template>
