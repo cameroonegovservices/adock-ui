6dc229bca57f425acc33f3d5602ca6e839baae52
Stéphane Raimbault <stephane.raimbault@gmail.com>
2018-03-19 16:07:09 +0100

    State the app is in Bêta

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
       router-link(:to="{name: 'search'}")
         img(src='./assets/marianne.svg', width='80px')
       v-toolbar-title A Dock - Simplifions les relations transporteurs/chargeurs
+      v-spacer
+      span Bêta
     v-content
       router-view
 </template>
