29ebf489fdc794d40d34f3e7b41a91de4d30a8bf
Stéphane Raimbault <stephane.raimbault@gmail.com>
2018-04-03 12:25:53 +0200

    New filter on license/weight

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,19 +1,46 @@
 <template lang="pug">
-  v-container(fluid, fill-height)
+  v-container(fluid fill-height)
     v-layout(justify-center)
       v-flex(xs12 md9 lg8 xl6)
         div.display-1.mt-4 Cherchez et contactez n'importe quel transporteur français
         v-alert(type="error" :value="error !== ''") {{ error }}
         v-text-field(
-          v-model="searchQuery",
-          label="Numéro de SIREN, SIRET ou nom de l'entreprise",
-          hint="Vous pouvez utiliser les premiers chiffres du SIRET ou le nom partiel de l'entreprise.",
+          v-model="searchQuery"
+          label="Numéro de SIREN, SIRET ou nom de l'entreprise"
+          hint="Vous pouvez utiliser les premiers chiffres du SIRET ou le nom partiel de l'entreprise."
           @keyup.enter="search"
         )
-        v-btn(large, color="primary", @click.native="search") Chercher
+        v-flex(xs6 md5 lg4 xl3)
+          v-select(
+            v-model="searchWeights"
+            label="Filtre sur le poids"
+            hint="Le transporteur doit disposer d'au moins une licence pour le critère."
+            chips
+            tags
+            :items="searchWeightChoices"
+            return-object
+          )
+            template(
+              slot="selection"
+              slot-scope="data"
+            )
+              v-chip.chip--select--multi(
+                @input="data.parent.selectItem(data.item)"
+                :selected="data.selected"
+                :disabled="data.disabled"
+                :key="JSON.stringify(data.item)"
+              )
+                v-avatar.accent {{ data.item.avatar }}
+                .
+                  {{ data.item.text }}
+        v-btn(large color="primary" @click.native="search") Chercher
         p(v-if="previousSearchQuery !== null && isSearching === false && transporteurs.length === 0 && error === ''") La recherche sur «&nbsp;{{ previousSearchQuery }}&nbsp;» n'a retourné aucun résultat.
         v-card.mt-1(v-if="transporteurs.length > 0")
-          transporteur-results(:searchQuery="previousSearchQuery", :transporteurs="transporteurs", :limit="limit")
+          transporteur-results(
+            :searchQuery="previousSearchQuery"
+            :transporteurs="transporteurs"
+            :limit="limit"
+          )
 </template>
 
 <script>
@@ -26,11 +53,27 @@ export default {
   data () {
     return {
       searchQuery: '',
+      searchWeights: [],
       previousSearchQuery: null,
       transporteurs: [],
       limit: 0,
       error: ''
     }
+  },
+
+  created () {
+    this.searchWeightChoices = [
+      {
+        text: 'Léger',
+        value: 'lti',
+        avatar: 'LTI'
+      },
+      {
+        text: 'Lourd',
+        value: 'lc',
+        avatar: 'LC'
+      }
+    ]
   },
 
   components: {
