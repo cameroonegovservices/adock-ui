b2c7aefb70d10a91a0beba39431fa0ec325b3624
Stéphane Raimbault <stephane.raimbault@gmail.com>
2018-04-09 20:32:52 +0200

    Reduce build size by 110kB for JS and 75kB for CSS with a-la-carte

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@
 import 'babel-polyfill'
 
 import Vue from 'vue'
-import Vuetify from 'vuetify'
+import { Vuetify } from 'vuetify'
+import vuetifyOptions from './vuetifyOptions'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -10,7 +11,7 @@ import store from './store'
 import './stylus/main.styl'
 
 Vue.config.productionTip = false
-Vue.use(Vuetify)
+Vue.use(Vuetify, vuetifyOptions)
 
 new Vue({
   router,
