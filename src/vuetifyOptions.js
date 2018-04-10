2bc1f53931111d435f362817d639c9b6c4932abf
Stéphane Raimbault <stephane.raimbault@gmail.com>
2018-04-10 22:25:38 +0200

    Forget to commit vuetifyOptions (CI becomes an urgent matter!)

---
+++ src/vuetifyOptions.js
@@ -0,0 +1,46 @@
+import {
+  transitions,
+  VApp,
+  VAlert,
+  VAvatar,
+  VBtn,
+  VCard,
+  VChip,
+  VDivider,
+  VFooter,
+  VGrid,
+  VIcon,
+  VList,
+  VSelect,
+  VSubheader,
+  VTextField,
+  VToolbar
+} from 'vuetify'
+
+import { Ripple } from 'vuetify/es5/directives'
+
+export const vuetifyOptions = {
+  components: {
+    transitions,
+    VApp,
+    VAlert,
+    VAvatar,
+    VBtn,
+    VCard,
+    VChip,
+    VDivider,
+    VFooter,
+    VGrid,
+    VIcon,
+    VList,
+    VSelect,
+    VSubheader,
+    VTextField,
+    VToolbar
+  },
+  directives: {
+    Ripple
+  }
+}
+
+export default vuetifyOptions
