802598874905486e0874eef5aa6be10fa536285f
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-04-16 14:45:01 +0200

    Use matomo-tracker but not sure is really browser compliant

--- src/router.js
+++ src/router.js
@@ -1,23 +1,42 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import MatomoTracker from 'matomo-tracker'
+
 import Search from './views/Search.vue'
 import Detail from './views/Detail.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const routes = [
+  {
+    path: '/',
+    name: 'search',
+    component: Search
+  },
+  {
+    path: '/transporteur/:transporteurSiret',
+    name: 'transporteur',
+    component: Detail,
+    props: true
+  }
+]
+
+const router = new Router({
   mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'search',
-      component: Search
-    },
-    {
-      path: '/transporteur/:transporteurSiret',
-      name: 'transporteur',
-      component: Detail,
-      props: true
-    }
-  ]
+  routes
 })
+
+// Enable Matomo tracker only for production
+if (process.env.NODE_ENV === 'production') {
+  const tracker = new MatomoTracker(50, '//stats.data.gouv.fr/piwik.php')
+
+  router.beforeEach((to, from, next) => {
+    tracker.track({
+      url: window.origin + '/#' + to.path,
+      title: to.name
+    })
+    next()
+  })
+}
+
+export default router
