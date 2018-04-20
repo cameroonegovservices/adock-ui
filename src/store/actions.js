bb77e9fa077f1d5f5e1749840f737cf0d854e216
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-04-20 11:04:56 +0200

    Upgrade actions to new api

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,12 +1,9 @@
-import Raven from 'raven-js'
 import api from '@/api.js'
 import * as types from './mutation-types'
 
 export const loadMeta = async ({ commit, state }) => {
-  try {
-    const response = await api.get('/meta/')
-    commit(types.SET_META, response.data)
-  } catch (error) {
-    Raven.captureException(error)
+  const meta = await api.getMeta()
+  if (meta) {
+    commit(types.SET_META, meta)
   }
 }
