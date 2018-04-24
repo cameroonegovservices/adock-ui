9b8d292ef1bacdda1e8da74fdc7a0b8454876913
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-04-24 19:56:12 +0200

    New actions object

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,12 @@
 import api from '@/api.js'
 
-export const loadMeta = async ({ commit, state }) => {
-  const meta = await api.getMeta()
-  if (meta) {
-    commit('SET_META', meta)
+export const actions = {
+  loadMeta: async ({ commit, state }) => {
+    const meta = await api.getMeta()
+    if (meta) {
+      commit('SET_META', meta)
+    }
   }
 }
+
+export default actions
