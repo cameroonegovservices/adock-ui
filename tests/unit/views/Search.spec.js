dd2311b690758569a7e5b890dc233539ef7cf4af
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-04-30 12:02:52 +0200

    Increase test coverage of Search view
    Now, running tests generates warnings:
    https://github.com/vuejs/vue-test-utils/issues/534

--- tests/unit/views/Search.spec.js
+++ tests/unit/views/Search.spec.js
@@ -1,11 +1,15 @@
 import Vuex from 'vuex'
 import Vuetify from 'vuetify'
-import { mount, createLocalVue } from '@vue/test-utils'
+import MockAdapter from 'axios-mock-adapter'
+import { mount, shallow, createLocalVue } from '@vue/test-utils'
 import deepClone from 'lodash.clonedeep'
 
+import { axiosInstance, searchTransporteursUrl } from '@/api'
 import { storeOptions } from '@/store/options'
 import Search from '@/views/Search.vue'
 import { addElementWithDataAppToBody } from '../utils'
+
+const mockAdapter = new MockAdapter(axiosInstance)
 
 describe('Search.vue', () => {
   const localVue = createLocalVue()
@@ -21,11 +25,50 @@ describe('Search.vue', () => {
     store = new Vuex.Store(clonedStoreOptions)
   })
 
+  afterEach(() => {
+    mockAdapter.reset()
+  })
+
   it('renders search', () => {
     const wrapper = mount(Search, {
       localVue,
       store
     })
     expect(wrapper.text()).toMatch('Chercher')
+  })
+
+  it('search transporteurs', async () => {
+    mockAdapter.onGet(searchTransporteursUrl)
+      .reply(200, {
+        results: [
+          {
+            raison_sociale: 'FOO'
+          }
+        ]
+      })
+
+    const wrapper = shallow(Search, {
+      localVue,
+      store
+    })
+    await wrapper.vm.search()
+    expect(wrapper.vm.transporteurs[0].raison_sociale).toBe('FOO')
+    expect(wrapper.vm.errors).toBe(null)
+    expect(wrapper.vm.limit).toBe(0)
+    expect(wrapper.vm.isSearchDone).toBe(true)
+  })
+
+  it('fails to search transporteurs', async () => {
+    mockAdapter.onGet(searchTransporteursUrl)
+      .reply(500)
+    const wrapper = shallow(Search, {
+      localVue,
+      store
+    })
+    await wrapper.vm.search()
+    expect(wrapper.vm.errors).toHaveLength(1)
+    expect(wrapper.vm.transporteurs).toEqual([])
+    expect(wrapper.vm.limit).toBe(0)
+    expect(wrapper.vm.isSearchDone).toBe(false)
   })
 })
