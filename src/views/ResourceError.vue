e85dcd9bba5516c336af6ece7dd3566d4a56eed2
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-06-04 11:17:02 +0200

    Slight change to error message

--- src/views/ResourceError.vue
+++ src/views/ResourceError.vue
@@ -15,7 +15,7 @@ v-container(fluid)
     v-layout(row justify-center align-center)
       v-flex(xs4)
         h3 Impossible de charger la ressource demandée.
-        p La cause est peut être une mauvaise connexion à Internet ou une indisponibilité du serveur.
+        p Cette erreur survient lors d'un problème de connexion à Internet ou d'une indisponibilité du serveur.
         v-btn(
           :to="errorUrl"
           color="primary"
