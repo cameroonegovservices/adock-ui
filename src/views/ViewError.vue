de4951681417539e1f9a06a43aee74615322d3f1
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-07-04 00:05:30 +0200

    Fix indent in ViewError template

--- src/views/ViewError.vue
+++ src/views/ViewError.vue
@@ -25,21 +25,21 @@ export default {
 
 <template lang="pug">
 v-container(fluid)
-    v-layout(row justify-center align-center)
-      v-flex(xs4)
-        h3 Impossible de charger la ressource demandée.
-        p(v-if="message") {{ message }}
-        p(v-else) Cette erreur survient lors d'un problème de connexion à Internet ou d'une indisponibilité du serveur.
-        v-btn(
-          v-if="fallbackUrl"
-          :to="fallbackUrl"
-          color="primary"
-        )
-          span(v-if="status === 404") Retour
-          span(v-else) Essayer à nouveau
-        v-btn(
-          v-else
-          @click="reload()"
-          color="primary"
-        ) Redémarrer l'application
+  v-layout(row justify-center align-center)
+    v-flex(xs4)
+      h3 Impossible de charger la ressource demandée.
+      p(v-if="message") {{ message }}
+      p(v-else) Cette erreur survient lors d'un problème de connexion à Internet ou d'une indisponibilité du serveur.
+      v-btn(
+        v-if="fallbackUrl"
+        :to="fallbackUrl"
+        color="primary"
+      )
+        span(v-if="status === 404") Retour
+        span(v-else) Essayer à nouveau
+      v-btn(
+        v-else
+        @click="reload()"
+        color="primary"
+      ) Redémarrer l'application
 </template>
