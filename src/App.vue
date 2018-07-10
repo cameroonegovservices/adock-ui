330adef777c1bb6e19e455dbdddde17154fc5a15
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-07-10 16:56:08 +0200

    Navigation drawer for mobile devices

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,12 @@ import Message from '@/components/Message.vue'
 export default {
   name: 'App',
 
+  data () {
+    return {
+      isDrawerVisible: false
+    }
+  },
+
   components: {
     Message
   },
@@ -30,6 +36,24 @@ export default {
 <template lang="pug">
   v-app
     Message
+    v-navigation-drawer(
+      app
+      clipped
+      v-model="isDrawerVisible"
+      absolute
+      temporary
+      right
+    )
+      v-list(dense)
+        v-list-tile(:to="{name: 'about'}")
+          v-list-tile-content
+            v-list-tile-title En savoir plus
+        v-list-tile(:to="{name: 'cgu'}")
+          v-list-tile-content
+            v-list-tile-title CGU
+        v-list-tile(href='https://www.ecologique-solidaire.gouv.fr/fabrique-numerique-lincubateur-services-numeriques-du-pole-ministeriel')
+          v-list-tile-content
+            v-list-tile-title La Fabrique Numérique
     v-toolbar.white(app light)
       v-tooltip(bottom)
         span(slot="activator")
@@ -41,11 +65,15 @@ export default {
       router-link(:to="{name: 'search'}")
         img.adock-app-name(src='./assets/adock.beta.gouv.fr-50.png' height='25px')
       v-spacer
-      v-toolbar-items
+      v-toolbar-side-icon(
+        v-if="$vuetify.breakpoint.xsOnly"
+        @click="isDrawerVisible = !isDrawerVisible"
+      )
+      v-toolbar-items(v-else)
         v-btn(flat :to="{name: 'about'}") En savoir plus
         v-btn(flat :to="{name: 'cgu'}") CGU
-      a(href='https://www.ecologique-solidaire.gouv.fr/fabrique-numerique-lincubateur-services-numeriques-du-pole-ministeriel')
-        img(src='./assets/logo-fabnum.svg' height='50px')
+        a(href='https://www.ecologique-solidaire.gouv.fr/fabrique-numerique-lincubateur-services-numeriques-du-pole-ministeriel')
+          img(src='./assets/logo-fabnum.svg' height='50px')
     v-content
       router-view
 </template>
