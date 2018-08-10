c75d30244c1633fbc7af3d224479ed73b0300ae8
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-08-10 11:05:50 +0200

    Remove useless container in search help

--- src/components/SearchHelp.vue
+++ src/components/SearchHelp.vue
@@ -1,24 +1,23 @@
 <template lang="pug">
   v-card.elevation-0.adock-search-help
-    v-container.pt-4
-      v-layout.adock-search-help(row wrap justify-space-around)
-        v-flex(xs3)
-          h2.text-xs-center
-            v-icon(x-large) search
-          h3.text-xs-center 1. Recherchez votre entreprise
-          p.text-xs-center Grâce à son SIRET ou son nom
-        v-flex(xs3)
-          h2.text-xs-center
-            v-icon(x-large) mode_edit
-          h3.text-xs-center 2. Complétez votre fiche transporteur
-          p.text-xs-center
-            | Avec le téléphone ou le mail de votre commercial
-            | et votre spécialité (par ex. transport frigorifique)
-        v-flex(xs3)
-          h2.text-xs-center
-            v-icon(x-large) call
-          h3.text-xs-center 3. Apparaissez en tête des recherches
-          p.text-xs-center Grâce à votre profil complet !
+    v-layout.pt-4.adock-search-help(row wrap justify-space-around)
+      v-flex(xs3)
+        h2.text-xs-center
+          v-icon(x-large) search
+        h3.text-xs-center 1. Recherchez votre entreprise
+        p.text-xs-center Grâce à son SIRET ou son nom
+      v-flex(xs3)
+        h2.text-xs-center
+          v-icon(x-large) mode_edit
+        h3.text-xs-center 2. Complétez votre fiche transporteur
+        p.text-xs-center
+          | Avec le téléphone ou le mail de votre commercial
+          | et votre spécialité (par ex. transport frigorifique)
+      v-flex(xs3)
+        h2.text-xs-center
+          v-icon(x-large) call
+        h3.text-xs-center 3. Apparaissez en tête des recherches
+        p.text-xs-center Grâce à votre profil complet !
 </template>
 
 <style lang="stylus">
