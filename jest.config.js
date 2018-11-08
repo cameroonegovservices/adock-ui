b56e9f991cf11b11876907df3337779b33d2f6d1
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-11-08 12:41:59 +0100

    Define the test URL for jest

--- jest.config.js
+++ jest.config.js
@@ -23,5 +23,6 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
+  testURL: 'http://localhost/',
   reporters: ['default', 'jest-unit']
 }
