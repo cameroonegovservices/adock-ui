a961d71ecbe7134e8d04ff0e84fc4d5761c4dbba
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-11-26 17:15:29 +0100

    Improve rendering of the map on Retina

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -260,7 +260,8 @@ export default {
         'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
         {
           maxZoom: 18,
-          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+          detectRetina: true
         }
       )
       tileLayer.addTo(map)
