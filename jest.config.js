0e01f7ad6e1ab7cc34fe30f820158d9f96226638
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-11-29 21:39:47 +0100

    No JSX files

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,13 @@ module.exports = {
   collectCoverage: true,
   moduleFileExtensions: [
     'js',
-    'jsx',
     'json',
     'vue'
   ],
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.jsx?$': 'babel-jest'
+    '^.+\\.js?$': 'babel-jest'
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
