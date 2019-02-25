8b0b17246066ea0ef6b23b42bda1822aece2cfcd
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2019-02-25 23:06:14 +0100

    Upgrade to Vuetify v1.5.2 and new "À la carte" build (yeah!)
    584k to 411k \o/

--- src/useVuetify.js
+++ src/useVuetify.js
@@ -1,99 +1,10 @@
 import Vue from "vue";
-// New Vuetify 1.3 can automatically import all the Vuetify components excepted
-// in PUG templates :(so we need to explicitly register our components.
-import Vuetify, {
-  VAlert,
-  VApp,
-  VAutocomplete,
-  VBtn,
-  VCard,
-  VCardActions,
-  VCardText,
-  VCardTitle,
-  VCheckbox,
-  VChip,
-  VCombobox,
-  VDataTable,
-  VDivider,
-  VFooter,
-  VForm,
-  VIcon,
-  VImg,
-  VList,
-  VListTile,
-  VListTileAction,
-  VListTileActionText,
-  VListTileContent,
-  VListTileSubTitle,
-  VListTileTitle,
-  VNavigationDrawer,
-  VProgressCircular,
-  VRadio,
-  VRadioGroup,
-  VSelect,
-  VSnackbar,
-  VSubheader,
-  VTextarea,
-  VTextField,
-  VToolbar,
-  VToolbarItems,
-  VToolbarSideIcon,
-  VToolbarTitle,
-  VTooltip
-} from "vuetify/lib";
-import VGrid from "vuetify/lib/components/VGrid/";
-import transitions from "vuetify/lib/components/transitions/";
-import { Ripple } from "vuetify/lib/directives";
+import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
-import fr from "vuetify/es5/locale/fr";
+import fr from "vuetify/lib/locale/fr";
 
 Vue.use(Vuetify, {
-  components: {
-    VApp,
-    VAlert,
-    VAutocomplete,
-    VBtn,
-    VCard,
-    VCardActions,
-    VCardText,
-    VCardTitle,
-    VCheckbox,
-    VChip,
-    VCombobox,
-    VDataTable,
-    VDivider,
-    VFooter,
-    VForm,
-    VGrid,
-    VIcon,
-    VImg,
-    VList,
-    VListTile,
-    VListTileAction,
-    VListTileActionText,
-    VListTileContent,
-    VListTileSubTitle,
-    VListTileTitle,
-    VNavigationDrawer,
-    VProgressCircular,
-    VRadio,
-    VRadioGroup,
-    VSelect,
-    VSnackbar,
-    VSubheader,
-    VTextField,
-    VTextarea,
-    VToolbar,
-    VToolbarItems,
-    VToolbarSideIcon,
-    VToolbarTitle,
-    VTooltip,
-    transitions
-  },
   customProperties: false,
-  directives: {
-    Ripple
-  },
   lang: {
     locales: { fr },
     current: "fr"
