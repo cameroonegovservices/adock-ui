1d3e649730fbb8e1c84d5696fbdbe1188e7cc725
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2019-03-01 16:20:12 +0100

    Load vuetify-loader for new À la carte build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
+const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
 module.exports = {
+  configureWebpack: {
+    plugins: [new VuetifyLoaderPlugin()]
+  },
   productionSourceMap: true,
   devServer: {
     allowedHosts: [".serveo.net"]
