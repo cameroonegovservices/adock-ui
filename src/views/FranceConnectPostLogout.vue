31788768bb571cccb6e3616a42f108250ecb4f24
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2019-03-27 08:49:51 +0100

    Add missing component FranceConnectPostLogout

---
+++ src/views/FranceConnectPostLogout.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  div
+</template>
+
+<script>
+export default {
+  name: "franceconnect-post-logout",
+
+  mounted() {
+    this.$router.push({ name: "search" });
+    this.$store.commit("MESSAGE_ADD", {
+      message: "L'utilisateur a été déconnecté de A Dock et de FranceConnect"
+    });
+  }
+};
+</script>
